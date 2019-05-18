30c166af0d773b3c6c5a1a855d76990e26d13ea5
Nicklas Utgaard <nutgaard@gmail.com>
2019-05-18 15:34:45 +0200

    [OPP-556] Basic styling og styring av listevisning

--- frontend/src/application.tsx
+++ frontend/src/application.tsx
@@ -4,12 +4,12 @@ import Teksterliste from "./components/teksterliste/teksterliste";
 import {Tekster} from "./model";
 import './application.less';
 
-const tekster: Tekster = {
-    id1: {id: 'id1', overskrift: 'Overskrift 1', tags: ['ks', 'arbeid'], innhold: {nb_NO: ''}},
-    id2: {id: 'id2', overskrift: 'Overskrift 2', tags: ['familie'], innhold: {nb_NO: ''}},
-    id3: {id: 'id3', overskrift: 'Overskrift 3', tags: ['ks', 'familie'], innhold: {nb_NO: ''}},
-    id4: {id: 'id4', overskrift: 'Overskrift 4', tags: ['ks'], innhold: {nb_NO: ''}},
-};
+const tekster: Tekster = new Array(50)
+    .fill(0)
+    .map((_, id) => ({
+        id: `id${id}`, overskrift: `Overskrift ${id}`, tags: ['ks', 'arbeid'], innhold: {nb_NO: ''}
+    }))
+    .reduce((acc, tekst) => ({ ...acc, [tekst.id]: tekst}), {});
 
 function Application() {
     return (
@@ -17,6 +17,7 @@ function Application() {
             <Header/>
             <div className="application__content">
                 <Teksterliste tekster={tekster}/>
+                <div className="application__editor">Content here</div>
             </div>
         </div>
     );
--- frontend/src/components/teksterliste/teksterliste.tsx
+++ frontend/src/components/teksterliste/teksterliste.tsx
@@ -1,49 +1,97 @@
-import React, {useCallback, useState} from 'react';
+import React, {DependencyList, Dispatch, SetStateAction, useCallback, useEffect, useState} from 'react';
+import { Input } from 'nav-frontend-skjema';
+import { Normaltekst } from 'nav-frontend-typografi';
+import classNames from 'classnames';
+import {Tekst, Tekster, UUID} from "../../model";
 import './teksterliste.less';
-import {Tekst, Tekster} from "../../model";
 
 interface Props {
     tekster: Tekster
 }
 
-function useFieldState(initialState: string | (() => string)): [string, React.ChangeEventHandler] {
+function cyclicgroup(size: number, value: number): number {
+    let v = value % size;
+    while (v < 0) { v += size; }
+    return v;
+}
+
+function useFieldState(initialState: string | (() => string)): [string, React.ChangeEventHandler, Dispatch<SetStateAction<string>>] {
     const [state, changeState] = useState(initialState);
     const onChange = useCallback(
         (event: React.ChangeEvent<HTMLInputElement>) => changeState(event.target.value),
         [changeState]
     );
 
-    return [state, onChange]
+    return [state, onChange, changeState]
 }
 
-function TekstListeElement(props: { tekst: Tekst }) {
+function useForceAllwaysInViewport(selector: string, deps: DependencyList = []) {
+    const query = React.useCallback(() => document.querySelector(selector), [selector]);
+    useEffect(() => {
+        query().scrollIntoView({ block: 'nearest', inline: 'nearest' });
+    }, [...deps, query]);
+}
+
+function TekstListeElement(props: { tekst: Tekst; checked: UUID, onChange: React.ChangeEventHandler }) {
+    const cls = classNames('teksterliste__listeelement', {
+        'teksterliste__listeelement--checked': props.tekst.id === props.checked
+    });
     return (
-        <li>
-            <span>{props.tekst.overskrift}</span>
-            <span>{props.tekst.tags.join(', ')}</span>
-        </li>
+        <label className={cls}>
+            <input
+                type="radio"
+                name="teksterliste__listeelement"
+                value={props.tekst.id}
+                checked={props.tekst.id === props.checked}
+                onChange={props.onChange}
+            />
+            <div className="teksterliste__listeelement-content">
+                <Normaltekst tag="span">{props.tekst.overskrift}</Normaltekst>
+                <Normaltekst tag="span">{props.tekst.tags.join(', ')}</Normaltekst>
+            </div>
+        </label>
     );
 }
 
-function matcherSok(tekst: Tekst, sok: string) {
+function matcherSok(tekst: Tekst, sok: string, checked: UUID) {
     const corpus = `${tekst.overskrift} ${tekst.tags.join(' ')} ${Object.values(tekst.innhold).join(' ')}`;
-    return corpus.toLocaleLowerCase().includes(sok.toLocaleLowerCase());
+    return tekst.id === checked || corpus.toLocaleLowerCase().includes(sok.toLocaleLowerCase());
 }
 
 function Teksterliste(props: Props) {
     const [sok, setSok] = useFieldState('');
+    const [checked, setChecked, setRawChecked] = useFieldState(Object.keys(props.tekster)[0] || '');
+    useForceAllwaysInViewport('.teksterliste__listeelement--checked', [checked]);
 
-    const tekster = Object.values(props.tekster)
-        .filter((tekst) => matcherSok(tekst, sok))
-        .map((tekst) => <TekstListeElement tekst={tekst} key={tekst.id}/>);
+
+    const tekster = Object.values(props.tekster).filter((tekst) => matcherSok(tekst, sok, checked));
+    const teksterRadios = tekster.map((tekst) => <TekstListeElement tekst={tekst} key={tekst.id} checked={checked} onChange={setChecked} />);
+
+    const keyHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        const noModifierKeys = [event.ctrlKey, event.shiftKey, event.altKey, event.metaKey].every((key) => !key);
+        if (noModifierKeys && ['ArrowUp', 'ArrowDown'].includes(event.key)) {
+            event.preventDefault();
+            const direction = event.key === 'ArrowUp' ? -1 : 1;
+            const indexOfCurrent = tekster.findIndex((tekst) => tekst.id === checked);
+            const newIndex = cyclicgroup(tekster.length, indexOfCurrent + direction);
+
+            setRawChecked(tekster[newIndex].id!);
+        }
+    };
 
     return (
-        <div className="teksterliste">
-            <input type="text" className="teksterliste__sok" value={sok} onChange={setSok}/>
-            <ol className="teksterliste__liste">
-                {tekster}
-            </ol>
-        </div>
+        <>
+            <Input
+                label="Søk"
+                className="teksterliste__sok"
+                value={sok}
+                onChange={setSok}
+                onKeyDown={keyHandler}
+            />
+            <div className="teksterliste__liste">
+                {teksterRadios}
+            </div>
+        </>
     );
 }
 
