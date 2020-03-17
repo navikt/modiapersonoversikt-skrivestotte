7903eb97f2039d5164225417d7a085ecce1278e4
Nicklas Utgaard <nutgaard@gmail.com>
2020-03-17 13:49:25 +0100

    [KAIZEN-0] bruker ukes-vindu for generering av statistikk for sortering

---
+++ src/main/kotlin/no/nav/modiapersonoversikt/service/LeaderElectorService.kt
@@ -0,0 +1,43 @@
+package no.nav.modiapersonoversikt.service
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.apache.Apache
+import io.ktor.client.request.get
+import kotlinx.coroutines.runBlocking
+import no.nav.modiapersonoversikt.Configuration
+import no.nav.modiapersonoversikt.measureTimeMillis
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.lang.Exception
+import java.net.InetAddress
+
+private val client = HttpClient(Apache)
+private val log: Logger = LoggerFactory.getLogger(LeaderElectorService::class.java)
+data class LeaderElectorResponse(val name: String)
+
+class LeaderElectorService(val configuration: Configuration) {
+    fun isLeader(): Boolean {
+        return measureTimeMillis("isLeader") {
+            if (configuration.clusterName == "local") {
+                true
+            } else {
+                val leader = getLeader()
+                val hostname = InetAddress.getLocalHost().hostAddress
+
+                hostname == leader.name
+            }
+
+        }
+    }
+
+    private fun getLeader(): LeaderElectorResponse {
+        return try {
+            runBlocking {
+                client.get<LeaderElectorResponse>(configuration.electorPath)
+            }
+        } catch (e: Exception) {
+            log.error("Could not get leader from ${configuration.electorPath}", e)
+            LeaderElectorResponse("")
+        }
+    }
+}
--- src/main/resources/db/migration/V1_2__statistics.sql
+++ src/main/resources/db/migration/V1_2__statistics.sql
@@ -1,3 +1,9 @@
+CREATE TABLE statistikk_raw(
+    id          BIGSERIAL   PRIMARY KEY     NOT NULL,
+    tidspunkt   TIMESTAMP   DEFAULT NOW()   NOT NULL,
+    tekstid     VARCHAR                     NOT NULL
+);
+
 CREATE TABLE statistikk(
     id          VARCHAR     PRIMARY KEY     NOT NULL,
     brukt       BIGINT      DEFAULT  0      NOT NULL
